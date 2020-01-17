b3a667dde9c7deacb64950bf697ffae4cb7cdb22
Sepideh <sepidehfallah2@gmail.com>
2020-01-17 14:07:39 -0700

    One time memoization and fetching each user only one time request to network

--- simple-blog/src/actions/index.js
+++ simple-blog/src/actions/index.js
@@ -13,14 +13,6 @@ import _ from "lodash";
 //   };
 // };
 
-export const fetchUser = id => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-  dispatch({
-    type: "FETCH_USER",
-    payload: response.data
-  });
-};
-
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   //Action
@@ -29,3 +21,27 @@ export const fetchPosts = () => async dispatch => {
     payload: response.data
   });
 };
+// before memoization
+// export const fetchUser = id => async dispatch => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   });
+// };
+
+//Memoizing the action Creator to avoid making multiple request to the network
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+//OneTime Memoization to avoid network request for 100 times.
+//_ at the begining of the function name means this is a private
+//function and other engineers should not make a request unless they
+//know what they are doing
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data
+  });
+});
